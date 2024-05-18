af1f4793e062243eba26a7a8e5633b2a924d3930
Bruno Oliveira <brunno.oliver7@gmail.com>
2024-05-18 00:55:54 -0300

    incrementando validation

--- src/main/java/bruno/rinhabackendjava/dto/PessoaDTO.java
+++ src/main/java/bruno/rinhabackendjava/dto/PessoaDTO.java
@@ -1,10 +1,12 @@
 package bruno.rinhabackendjava.dto;
 
+import bruno.rinhabackendjava.validator.ValidDate;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
-import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
@@ -15,12 +17,18 @@ public class PessoaDTO {
     private UUID id;
 
     @NotNull
+    @NotEmpty
+    @Max(32)
     private String apelido;
 
     @NotNull
+    @NotEmpty
+    @Max(100)
     private String nome;
 
-    private LocalDate nascimento;
+    @NotNull
+    @ValidDate
+    private String nascimento;
 
     @NotNull
     private List<String> stack;
