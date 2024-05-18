37c0c3f485c1f9b77c82bf0a9669e882331984fc
Bruno Oliveira <brunno.oliver7@gmail.com>
2024-05-18 00:57:29 -0300

    ajustando script inicial do banco, criando indice pg_trgm

--- db/init.sql
+++ db/init.sql
@@ -1,10 +1,16 @@
 CREATE SCHEMA rinha;
 
--- CREATE TABLE IF NOT EXISTS rinha.pessoa
--- (
---     id         uuid         not null primary key,
---     apelido    varchar(64)  not null unique,
---     nome       varchar(200) not null,
---     nascimento timestamp    not null,
---     stack      VARCHAR[]    null default '{}'
--- );
+CREATE TABLE IF NOT EXISTS rinha.pessoa
+(
+    id         uuid         not null primary key,
+    apelido    varchar(32)  not null unique,
+    nome       varchar(100) not null,
+    nascimento varchar(10)  not null,
+    stack      text         null,
+    version    int,
+    termo     TEXT GENERATED ALWAYS AS (nome || apelido || stack) STORED
+);
+
+CREATE EXTENSION IF NOT EXISTS pg_trgm;
+
+CREATE INDEX IF NOT EXISTS trgm_idx ON rinha.pessoa USING gist (termo gist_trgm_ops);
