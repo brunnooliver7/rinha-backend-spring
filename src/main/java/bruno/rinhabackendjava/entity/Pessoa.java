5594e8dceeb86e5581cd1d2ad3213d84e1efdaef
Bruno Oliveira <brunno.oliver7@gmail.com>
2024-05-18 00:56:04 -0300

    alterando nascimento para String

--- src/main/java/bruno/rinhabackendjava/entity/Pessoa.java
+++ src/main/java/bruno/rinhabackendjava/entity/Pessoa.java
@@ -5,17 +5,12 @@ import lombok.Setter;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
-import java.time.LocalDate;
 import java.util.UUID;
 
 @Getter
 @Setter
 @Entity
-@Table(schema = "rinha", name = "pessoa", indexes = {
-        @Index(columnList = "id", name = "idx_id"),
-        @Index(columnList = "apelido", name = "idx_apelido"),
-        @Index(columnList = "nome", name = "idx_nome"),
-        @Index(columnList = "stack", name = "idx_stack")})
+@Table(schema = "rinha", name = "pessoa")
 public class Pessoa {
 
     @Id
@@ -29,7 +24,7 @@ public class Pessoa {
     @Column(nullable = false)
     private String nome;
 
-    private LocalDate nascimento;
+    private String nascimento;
 
     private String stack;
 
